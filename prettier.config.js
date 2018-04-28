d27f1eae8f05dd89bd6070f2d0dae033e64abcd3
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-04-29 01:43:56 +0300

    First commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// .prettierrc.js
+module.exports = {
+  printWidth: 80,
+  // parser: 'flow',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true
+};
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// webpack v4
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+        options: {
+          // several examples !
+          // default value
+          formatter: require('eslint/lib/formatters/stylish')
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+};
